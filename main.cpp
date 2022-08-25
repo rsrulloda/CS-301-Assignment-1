754c7ff2423b7af96acac382d699f9caa5258049
JohnD56 <112040011+JohnD56@users.noreply.github.com>
2022-08-25 15:45:15 -0700

    Added my own version of main

--- main.cpp
+++ main.cpp
@@ -1,14 +1,71 @@
 //
-// Created by Alexandro I. Milanez on 8/25/22.
-//
+//John Delgado
+//main.cpp 
+//assignment 1
+// August 24, 2022
+
 #include <iostream>
+
 #include "number_bucket.h"
 
 using namespace std;
 
 int main() {
-    number_bucket bucket;
-    bucket.get_size(0);
 
-    return 0;
+number_bucket stackTest; //declare the instance of stack
+
+char ch;
+
+int n;
+
+while(ch!='n')
+
+{
+
+cout<<"\nEnter choice\n 1. Check stack \n 2. push\n 3.pop\n 4. check size\n";
+
+cin>> ch;
+
+switch(ch)
+
+{
+
+case '1':
+
+stackTest.ifEmpty();
+
+break;
+
+case '2':
+
+cout<<"Enter desired nue: ";
+
+cin >> n;
+
+stackTest.push(n);
+
+break;
+
+case '3':
+
+stackTest.pop();
+
+break;
+
+case '4':
+
+stackTest.get_size();
+
+break;
+
+}
+
+cout<<"\nContinue? (y/n)";
+
+cin>> ch;
+
+}
+
+return 0;
+
 }
