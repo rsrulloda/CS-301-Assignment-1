113f566c90fe4b34184687f0066498c433d00281
rsrulloda <ronell.rulloda@gmail.com>
2022-08-24 17:53:31 -0700

    Initial Commit

---
+++ number_bucket.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ronel on 8/24/2022.
+//
+
+#include "number_bucket.h"
---
+++ number_bucket.h
@@ -0,0 +1,17 @@
+#ifndef ASSIGNMENT_1_NUMBER_BUCKET_H
+#define ASSIGNMENT_1_NUMBER_BUCKET_H
+
+class number_bucket {
+public:
+    number_bucket();
+
+    bool ifEmpty();
+    void push(int n);
+    void pop();
+    int get_size();
+
+private:
+
+};
+
+#endif //ASSIGNMENT_1_NUMBER_BUCKET_H
